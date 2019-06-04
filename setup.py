7ea9e13084e672279b3854b82a7ec3b490eff7a5
Ben Zercher <bzercher@uw.edu>
2019-06-04 14:15:18 -0700

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='uimfpy',
+      versions='1.0',
+      packages=['uimfpy'],
+      py_modules=['MassRange.py','MzCalibrator.py','UIMFReader.py'],
+      url='https://github.com/bzercher/uimfpy.git'
+      )
+
+

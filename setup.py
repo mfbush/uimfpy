9e32653ca6ff6194b5ad21282e11ff296bb91cd5
Matt Bush <48997577+mfbush@users.noreply.github.com>
2020-01-26 20:04:44 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(name='uimfpy',
       versions='1.0',
       packages=['uimfpy'],
-      url='https://github.com/bzercher/uimfpy.git'
+      url='https://github.com/mfbush/uimfpy.git'
       )
 
 
